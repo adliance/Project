f7a2499ac5afa0e8a20ecb1213476efefe95b87c
Adrian Fürschuß <adrian.fuerschuss@adliance.net>
2022-05-03 13:56:02 +0200

    Add user info endpoint.

---
+++ src/Adliance.Project.Server.Web/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Claims;
+using Adliance.Project.Shared.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adliance.Project.Server.Web.Controllers;
+
+[ApiController, Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    [Authorize, AllowAnonymous]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetCurrentUser()
+    {
+        return Ok(CreateUserInfo(User));
+    }
+
+    private static UserInfo CreateUserInfo(ClaimsPrincipal claimsPrincipal)
+    {
+        if (claimsPrincipal.Identity == null || !claimsPrincipal.Identity.IsAuthenticated)
+        {
+            return UserInfo.Anonymous;
+        }
+
+        var userInfo = new UserInfo
+        {
+            IsAuthenticated = true
+        };
+
+        if (claimsPrincipal.Identity is ClaimsIdentity claimsIdentity)
+        {
+            userInfo.NameClaimType = claimsIdentity.NameClaimType;
+            userInfo.RoleClaimType = claimsIdentity.RoleClaimType;
+        }
+        else
+        {
+            userInfo.NameClaimType = ClaimTypes.Name;
+            userInfo.RoleClaimType = ClaimTypes.Role;
+        }
+
+        if (claimsPrincipal.Claims.Any())
+        {
+            var claims = new List<ClaimValue>();
+            var nameClaims = claimsPrincipal.FindAll(userInfo.NameClaimType);
+            foreach (var claim in nameClaims)
+            {
+                claims.Add(new ClaimValue(userInfo.NameClaimType, claim.Value));
+            }
+
+            foreach (var claim in claimsPrincipal.Claims.Except(nameClaims))
+            {
+                claims.Add(new ClaimValue(claim.Type, claim.Value));
+            }
+
+            userInfo.Claims = claims;
+        }
+
+        return userInfo;
+    }
+}
---
+++ src/Adliance.Project.Shared/Authorization/ClaimValue.cs
@@ -0,0 +1,14 @@
+﻿namespace Adliance.Project.Shared.Authorization;
+
+public class ClaimValue
+{
+    public ClaimValue(string type, string value)
+    {
+        Type = type;
+        Value = value;
+    }
+
+    public string Type { get; set; }
+
+    public string Value { get; set; }
+}
